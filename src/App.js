b81b011fde1d798b1d45f34ef0423eea3ed2d3ef
Vasif Mammadov <vasif.will@gmail.com>
2022-07-29 11:32:09 -0700

    form handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const initialValues = {email:"", password:""}
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({})
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setFormValues({...formValues, [name]: value})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormErrors(validate(formValues))
+  }
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    if(!values.email){
+      errors.email = "email is required"
+    }
+    if(!values.password){
+      errors.password = "password is required"
+    }
+    return errors
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit} >
+    <input type="text" placeholder='Email' name="email"  value={formValues.email} onChange={handleChange}/>
+    <span>{formErrors.email}</span>
+    <input type="password" placeholder='password' name="pasword" value={formValues.password} onChange={handleChange}/>
+    <span>{formErrors.password}</span>
+    <input  type="submit"/>
+
+    </form>
+     
     </div>
   );
 }
